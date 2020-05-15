114d67dc3a8aa88912b47f2dd4cb63bb9ce98f64
yskang <kys061@gmail.com>
2020-05-15 15:29:35 +0900

    add getting total flows by limit and sorted avg rate

--- flow_recorder8.0.py
+++ flow_recorder8.0.py
@@ -16,6 +16,7 @@ import csv
 import os
 import sys
 import re
+from pprint import pprint
 
 
 USER = 'cli_admin'
@@ -25,31 +26,28 @@ PORT = '5000'
 REST_BASIC_PATH ='configurations/running/'
 REST_FLOW_PATH = 'flows/'
 FLOW_CSV_FILENAME = '{}{}{}_{}_{}_flows.log' # year, mon, day, {with}, ip
+FLOW_TOT_CSV_FILENAME = '{}{}{}_total_flows.log' # year, mon, day, {with}, ip
 RECORDER_LOG_FILENAME = r'/var/log/flow_recorder8.0.log'
 FLOW_PATH = '/var/log/flows/'
 TOKEN = '1'
 ORDER = '<average_rate'
 START = '0'
-LIMIT = '100000'
+LIMIT = '10000'
 WITH = 'with='  # with=dest_host=ipaddress
 WITH_ATTR = [
 'dest_host',
 'source_host',
 ]
 OUTPUT_FILENAME = '/var/log/flow.log'
-
 logger_recorder = None
-
-
 INCLUDE = [
 '103.194.111.4',
-'103.194.111.5',
-'103.194.111.6',
-'103.194.111.7',
-'133.186.160.28',
-'133.186.160.29',
+# '103.194.111.5',
+# '103.194.111.6',
+# '103.194.111.7',
+# '133.186.160.28',
+# '133.186.160.29',
 ]
-
 FLOW_ATTR = [
 'name',
 'ingress_interface',
@@ -151,20 +149,37 @@ def make_flow_folder(year, mon, day):
         return path_day
 
 
-def get_rest_url(ip, flow_attrs, _with_attr):
-    return "{}{}?token={}&order={}&start={}&limit={}&select={}&with={}={}".format(
-        REST_BASIC_PATH, REST_FLOW_PATH, TOKEN, ORDER, START, LIMIT, flow_attrs, _with_attr, ip)
+def get_rest_url(ip, flow_attrs, _with_attr, has_with=False):
+    # print("{}{}?token={}&order={}&start={}&limit={}&select={}&with={}={}".format(
+    #     REST_BASIC_PATH, REST_FLOW_PATH, TOKEN, ORDER, START, LIMIT, flow_attrs, _with_attr, ip))
+    if has_with:
+        return "{}{}?token={}&order={}&start={}&limit={}&select={}&with={}={}".format(
+            REST_BASIC_PATH, REST_FLOW_PATH, TOKEN, ORDER, START, LIMIT, flow_attrs, _with_attr, ip)
+    else:
+        return "{}{}?token={}&order={}&start={}&limit={}&select={}".format(
+            REST_BASIC_PATH, REST_FLOW_PATH, TOKEN, ORDER, START, LIMIT, flow_attrs)
+
 
 def get_flows(rest_flow_url):
     api_start = time.time()
+
     try:
-        parsed_rest_url = urlparse(rest_flow_url)
-        coll_flows = api.rest.get(rest_flow_url)['collection']
-        parsed_qs = parse_qs(parsed_rest_url.query)
-        ip = parsed_qs['with'][0].split('=')[1]
-        # logger_recorder.info('api elapsed: {}'.format(time.time() - api_start))
-        logger_recorder.info('collection count of {0} : {1}, it takes {2:.2f} seconds'.format(ip, len(coll_flows), time.time() - api_start))
-        # for_start = time.time()
+        # print(rest_flow_url)
+        if re.search('with', rest_flow_url):
+            parsed_rest_url = urlparse(rest_flow_url)
+            # pprint(parsed_rest_url)
+            coll_flows = api.rest.get(rest_flow_url)['collection']
+            parsed_qs = parse_qs(parsed_rest_url.query)
+            # pprint(parsed_qs)
+            ip = parsed_qs['with'][0].split('=')[1]
+            # logger_recorder.info('api elapsed: {}'.format(time.time() - api_start))
+            logger_recorder.info('collection count of {0} : {1}, it takes {2:.2f} seconds'.format(ip, len(coll_flows), time.time() - api_start))
+            # for_start = time.time()
+        else:
+            # import pdb
+            # pdb.set_trace()
+            coll_flows = api.rest.get(rest_flow_url)['collection']
+            logger_recorder.info('collection count of {0} : {1}, it takes {2:.2f} seconds'.format("total flows", len(coll_flows), time.time() - api_start))
     except Exception as e:
         logger_recorder.error('get_flows: {}'.format(e))
     else:
@@ -181,23 +196,36 @@ def get_flows(rest_flow_url):
                     flow[key] = 'none'
                 # logger_recorder.info(type(flow[key]))
         # logger_recorder.info('for elapsed: {}'.format(time.time() - for_start))
-        return {
-            'coll_flows': coll_flows,
-            'parsed_qs': parse_qs(parsed_rest_url.query)
-        }
+        if re.search('with', rest_flow_url):
+            return {
+                'coll_flows': coll_flows,
+                'parsed_qs': parse_qs(parsed_rest_url.query)
+            }
+        else:
+            return {
+                'coll_flows': coll_flows,
+            }
 
 
 def write_flows(coll_flows):
     writer_start = time.time()
     today = time.localtime()
     path_day = make_flow_folder(str(today.tm_year), str(today.tm_mon), str(today.tm_mday))
+    has_with = False
     try:
-        flows = coll_flows['coll_flows']
-        count_of_flows = len(flows)
-        parsed_qs = coll_flows['parsed_qs']
-        _with = parsed_qs['with'][0].split('=')[0]
-        ip = parsed_qs['with'][0].split('=')[1]
-        flow_csv_filepath = path_day + FLOW_CSV_FILENAME.format(today.tm_year, today.tm_mon, today.tm_mday, _with, ip)
+        if 'parsed_qs' in coll_flows:
+            flows = coll_flows['coll_flows']
+            count_of_flows = len(flows)
+            parsed_qs = coll_flows['parsed_qs']
+            _with = parsed_qs['with'][0].split('=')[0]
+            ip = parsed_qs['with'][0].split('=')[1]
+            flow_csv_filepath = path_day + FLOW_CSV_FILENAME.format(today.tm_year, today.tm_mon, today.tm_mday, _with, ip)
+            has_with = True
+        else:
+            flows = coll_flows['coll_flows']
+            count_of_flows = len(flows)
+            flow_csv_filepath = path_day + FLOW_TOT_CSV_FILENAME.format(today.tm_year, today.tm_mon, today.tm_mday)
+            has_with = False
     except Exception as e:
         logger_recorder.error('write_flows: {}'.format(e))
         pass
@@ -207,7 +235,10 @@ def write_flows(coll_flows):
                 writer = csv.DictWriter(f, fieldnames=FIELD_NAMES)
                 writer.writeheader()
                 for flow in flows:
-                    if str(flow[_with]) in include_subnet_tree:
+                    if has_with:
+                        if str(flow[_with]) in include_subnet_tree:
+                            writer.writerow(flow)
+                    else:
                         writer.writerow(flow)
                     # if str(flow['source_host']) in include_subnet_tree:
                     #     writer.writerow(flow)
@@ -218,18 +249,14 @@ def write_flows(coll_flows):
             with open(flow_csv_filepath, 'a') as f:
                 writer = csv.DictWriter(f, fieldnames=FIELD_NAMES)
                 for flow in flows:
-                    if str(flow[_with]) in include_subnet_tree:
+                    if has_with:
+                        if str(flow[_with]) in include_subnet_tree:
+                            writer.writerow(flow)
+                    else:
                         writer.writerow(flow)
-                    # if str(flow['source_host']) in include_subnet_tree:
-                    #     writer.writerow(flow)
+
             logger_recorder.info('{0} Flows are updated to {1}, it takes {2:.2f} seconds'.format(
                 count_of_flows, flow_csv_filepath, time.time() - writer_start))
-            # logger_recorder.info('writer elapsed: {}'.format(time.time() - writer_start))
-            # print_line()
-        # sleep_start = time.time()
-        # time.sleep(10)
-        # logger_recorder.info('writer sleep elapsed: {}'.format(time.time() - sleep_start))
-        # print_line()
 
 
 def print_line():
@@ -251,14 +278,11 @@ def get_and_wirte_flows():
             del flow['_key_order']
             del flow['link']
             del flow['class']
-            # for key in flow:
-            #     logger_recorder.info(key)
             for key in flow:
                 if isinstance(flow[key], dict):
                     flow[key] = flow[key]['link']['name']
                 if flow[key] == '':
                     flow[key] = 'none'
-                # logger_recorder.info(type(flow[key]))
         logger_recorder.info('for elapsed: {}'.format(time.time() - for_start))
         writer_start = time.time()
         with open('/home/saisei/dev/flow_recorder8.0/test_api.txt', 'a') as f:
@@ -273,7 +297,7 @@ def get_and_wirte_flows():
         print_line()
 
 try:
-    if re.search('flow_recorder.py', sys.argv[0]):
+    if re.search('flow_recorder8.0.py', sys.argv[0]):
         make_logger()
 except Exception as e:
     pass
@@ -305,53 +329,41 @@ def main():
         sys.exit()
     else:
         while True:
-            for i in range(len(INCLUDE)):
+            # the count of range have to add 1, because of total rest get.
+            for i in range(len(INCLUDE)+1):
                 tra = ThreadRestApi(queue, out_queue)
                 tra.daemon = True
                 tra.start()
 
             logger_recorder.info('Getting Started Collecting...')
-            for idx, ip in enumerate(INCLUDE):
+            for idx, ip in enumerate(INCLUDE, 1):
                 logger_recorder.info('{} : {}'.format(idx, ip))
                 for _with_attr in WITH_ATTR:
-                    rest_flow_url = get_rest_url(ip, flow_attrs, _with_attr)
+                    rest_flow_url = get_rest_url(ip, flow_attrs, _with_attr, has_with=True)
                     queue.put(rest_flow_url)
+            logger_recorder.info('{} : {}'.format(len(INCLUDE)+1, "total {} flows by sorting avg rate".format(LIMIT)))
+            rest_flow_tot_url = get_rest_url("", flow_attrs, "", has_with=False)
+            queue.put(rest_flow_tot_url)
             print_line()
 
-            for i in range(len(INCLUDE)):
+            for i in range(len(INCLUDE)+1):
                 tw = ThreadWriter(out_queue)
                 tw.daemon = True
                 tw.start()
 
             queue.join()
             out_queue.join()
-            # threads = []
-            # t = threading.Thread(target=get_and_wirte_flows())
-            # threads.append(t)
-            # for t in threads:
-            #     t.start()
-            # for t in threads:
-            #     t.join()
+
             sleep_start = time.time()
             time.sleep(10)
             logger_recorder.info('sleep elapsed: {0:.2f}'.format(time.time() - sleep_start))
             print_line()
 
+
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print ("\r\nThe script is terminated by user interrupt!")
-        print ("Bye!!")
+        print("\r\nThe script is terminated by user interrupt!")
+        print("Bye!!")
         sys.exit()
-
-# plogger_recorder.info(coll_flows)
-
-# def type_checker(key, data):
-#     if isinstance(data, dict):
-#         data[]
-#     if isinstance(data, unicode):
-#         if data is 'name':
-#             return data
-#         else:
-#             return 'none'
